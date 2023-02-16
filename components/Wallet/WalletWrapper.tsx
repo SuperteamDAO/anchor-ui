5783f985c90b8c6d94cc39b0932e8d5f5b9e28ad
0xPratik <sariapratik@gmail.com>
2023-02-16 16:36:09 +0530

    fix: clean up

--- components/Wallet/WalletWrapper.tsx
+++ components/Wallet/WalletWrapper.tsx
@@ -1,11 +1,9 @@
 import React, { Children, useMemo } from 'react'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter, UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 require('@solana/wallet-adapter-react-ui/styles.css');
 
